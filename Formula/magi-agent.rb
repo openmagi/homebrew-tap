a5ce9b9433e7f84abe1f846b377e27f8039fa2e7
kevin-hs-sohn <48140360+kevin-hs-sohn@users.noreply.github.com>
2026-06-03 21:13:43 +0000

    magi-agent: add 0.1.5 bottle.

--- Formula/magi-agent.rb
+++ Formula/magi-agent.rb
@@ -7,6 +7,12 @@ class MagiAgent < Formula
   sha256 "4037a5b3f0ceaaf45c3bbf2397c5ed7fbb22494c70a8d9309e0eb9c6e14a2baa"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://github.com/openmagi/homebrew-tap/releases/download/magi-agent-0.1.5"
+    sha256               arm64_tahoe:  "7a197a67416cf7885b2ac4f74ab3400c22d12ec2d18f41e68770908ba7729fc3"
+    sha256 cellar: :any, x86_64_linux: "f4913cd8665f7545406b4ee36938f20ba945f8fc542f63e1134cb8ce0243ead9"
+  end
+
   depends_on "python@3.13"
 
   def install
