a1c2cdc4b60bfea50dd80aed6c59431eb2637e08
Kevin <kevin.hs.sohn@gmail.com>
2026-06-04 18:21:40 -0700

    Update magi-agent formula to 0.1.8
    Closes #10.
    
    Signed-off-by: kevin-hs-sohn <48140360+kevin-hs-sohn@users.noreply.github.com>

--- Formula/magi-agent.rb
+++ Formula/magi-agent.rb
@@ -3,15 +3,9 @@ class MagiAgent < Formula
 
   desc "Open Magi agent runtime and CLI"
   homepage "https://github.com/openmagi/magi-agent"
-  url "https://github.com/openmagi/magi-agent/releases/download/v0.1.7/magi_agent-0.1.7.tar.gz"
-  sha256 "58c5a4e1b2f49531efcd43f96a2fb3dbde765233cec8c6feb23a050de851385a"
+  url "https://github.com/openmagi/magi-agent/releases/download/v0.1.8/magi_agent-0.1.8.tar.gz"
+  sha256 "e8fd1dd7365d32581efb7b34f6ff9073486c863d6666a057005480cca28201a2"
   license "Apache-2.0"
-
-  bottle do
-    root_url "https://github.com/openmagi/homebrew-tap/releases/download/magi-agent-0.1.7"
-    sha256               arm64_tahoe:  "f3795cf47b29a5375d720346956406dab32cb157f3081132ef57b1ed6e698b25"
-    sha256 cellar: :any, x86_64_linux: "53774cf2d6846f176dec3cc0a1b052c17b479c567a72a0b8a54779a250514263"
-  end
 
   depends_on "python@3.13"
 
