d7862b8b843b6b2111403dc70aae249e41613afe
Kevin <kevin.hs.sohn@gmail.com>
2026-06-05 05:03:54 -0700

    magi-agent 0.1.12
    Closes #15.
    
    Signed-off-by: kevin-hs-sohn <48140360+kevin-hs-sohn@users.noreply.github.com>

--- Formula/magi-agent.rb
+++ Formula/magi-agent.rb
@@ -3,16 +3,9 @@ class MagiAgent < Formula
 
   desc "Open Magi agent runtime and CLI"
   homepage "https://github.com/openmagi/magi-agent"
-  url "https://github.com/openmagi/magi-agent/releases/download/v0.1.11/magi_agent-0.1.11.tar.gz"
-  sha256 "5dd9a84699b2dbfc1590237204ddb7388f8b634e98e6ce5b0b2c225c7985c850"
+  url "https://github.com/openmagi/magi-agent/releases/download/v0.1.12/magi_agent-0.1.12.tar.gz"
+  sha256 "c04606df977c3687df0157fa7f87bd2d570f88ea57b4eea9e4790d2e6a06bc1b"
   license "Apache-2.0"
-  revision 1
-
-  bottle do
-    root_url "https://github.com/openmagi/homebrew-tap/releases/download/magi-agent-0.1.11_1"
-    sha256               arm64_tahoe:  "3d3a9f1a086f96f4f481a22d07da4d5291191da87ad98ae3c096a4825a5bd45a"
-    sha256 cellar: :any, x86_64_linux: "8bac37af79a6b90622c014ca19103051ed5fbdd6f76baa3e6784988114acef43"
-  end
 
   depends_on "python@3.13"
 
