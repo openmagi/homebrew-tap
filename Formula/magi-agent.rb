d56dd64ba755d1728bfa7580f4faaf31f8ae3a53
Kevin <kevin.hs.sohn@gmail.com>
2026-06-04 23:17:31 -0700

    magi-agent 0.1.9

--- Formula/magi-agent.rb
+++ Formula/magi-agent.rb
@@ -3,15 +3,9 @@ class MagiAgent < Formula
 
   desc "Open Magi agent runtime and CLI"
   homepage "https://github.com/openmagi/magi-agent"
-  url "https://github.com/openmagi/magi-agent/releases/download/v0.1.8/magi_agent-0.1.8.tar.gz"
-  sha256 "e8fd1dd7365d32581efb7b34f6ff9073486c863d6666a057005480cca28201a2"
+  url "https://github.com/openmagi/magi-agent/releases/download/v0.1.9/magi_agent-0.1.9.tar.gz"
+  sha256 "a8f7b525cd2620f5c08ecde38bee030393efde3fa3872e490f7a764ed464b217"
   license "Apache-2.0"
-
-  bottle do
-    root_url "https://github.com/openmagi/homebrew-tap/releases/download/magi-agent-0.1.8"
-    sha256               arm64_tahoe:  "04245f9bad57fb6b0a04665c215bd61f5662c0161d1876b3d825cad15ebe1f4a"
-    sha256 cellar: :any, x86_64_linux: "129c7587509f21d7e98419d6e59394c33ba5c39defaa2c2251b33e272f8a47cb"
-  end
 
   depends_on "python@3.13"
 
