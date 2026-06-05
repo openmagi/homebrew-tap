bec1f428e40965787a5c5bd6037e9914a89765d4
Kevin <kevin.hs.sohn@gmail.com>
2026-06-05 04:20:32 -0700

    magi-agent 0.1.11

--- Formula/magi-agent.rb
+++ Formula/magi-agent.rb
@@ -3,21 +3,15 @@ class MagiAgent < Formula
 
   desc "Open Magi agent runtime and CLI"
   homepage "https://github.com/openmagi/magi-agent"
-  url "https://github.com/openmagi/magi-agent/releases/download/v0.1.10/magi_agent-0.1.10.tar.gz"
-  sha256 "3541cd1772d640d352ea7f4b02b9ffd85251fcd279026a2c2de2dc3aece3649f"
+  url "https://github.com/openmagi/magi-agent/releases/download/v0.1.11/magi_agent-0.1.11.tar.gz"
+  sha256 "5dd9a84699b2dbfc1590237204ddb7388f8b634e98e6ce5b0b2c225c7985c850"
   license "Apache-2.0"
-
-  bottle do
-    root_url "https://github.com/openmagi/homebrew-tap/releases/download/magi-agent-0.1.10"
-    sha256               arm64_tahoe:  "ea898a5f5b2c9fedb1151b7336d9a6791acfaeda317a766cf4ce538f927f0b5a"
-    sha256 cellar: :any, x86_64_linux: "c5687d76b4adb1c8c2839ab6ea900e1789315617cc61df6c2573b05df00d6357"
-  end
 
   depends_on "python@3.13"
 
   def install
     virtualenv_create(libexec, "python3.13")
-    system libexec/"bin/python", "-m", "pip", "install", "--disable-pip-version-check", "#{buildpath}[cli]"
+    system libexec/"bin/python", "-m", "pip", "install", "--disable-pip-version-check", "#{buildpath}[cli,providers]"
     bin.install_symlink libexec/"bin/magi"
     bin.install_symlink libexec/"bin/magi-agent"
   end
