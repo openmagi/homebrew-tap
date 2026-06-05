64212c5dfcb49c2942c14e4c75c7128ec0c0f6af
Kevin <kevin.hs.sohn@gmail.com>
2026-06-05 04:34:17 -0700

    magi-agent 0.1.11_1
    Closes #14.
    
    Signed-off-by: kevin-hs-sohn <48140360+kevin-hs-sohn@users.noreply.github.com>

--- Formula/magi-agent.rb
+++ Formula/magi-agent.rb
@@ -6,17 +6,13 @@ class MagiAgent < Formula
   url "https://github.com/openmagi/magi-agent/releases/download/v0.1.11/magi_agent-0.1.11.tar.gz"
   sha256 "5dd9a84699b2dbfc1590237204ddb7388f8b634e98e6ce5b0b2c225c7985c850"
   license "Apache-2.0"
-
-  bottle do
-    root_url "https://github.com/openmagi/homebrew-tap/releases/download/magi-agent-0.1.11"
-    sha256 cellar: :any, x86_64_linux: "05d18815cd4d63a1a69457ebbb6ae20186cf7a8fd7b2746596eb0025a3f9c079"
-  end
+  revision 1
 
   depends_on "python@3.13"
 
   def install
     virtualenv_create(libexec, "python3.13")
-    system libexec/"bin/python", "-m", "pip", "install", "--disable-pip-version-check", "#{buildpath}[cli,providers]"
+    system libexec/"bin/python", "-m", "pip", "install", "--disable-pip-version-check", "#{buildpath}[cli]"
     bin.install_symlink libexec/"bin/magi"
     bin.install_symlink libexec/"bin/magi-agent"
   end
