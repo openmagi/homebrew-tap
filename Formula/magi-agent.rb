b6325b1136af3d46358bd677b96e623dc9689fae
Kevin <kevin.hs.sohn@gmail.com>
2026-06-06 13:47:20 -0700

    magi-agent 0.1.19 bottles

--- Formula/magi-agent.rb
+++ Formula/magi-agent.rb
@@ -7,6 +7,12 @@ class MagiAgent < Formula
   sha256 "807ec4d3744a68b0bac64a99eb2b8c22548bc10d6ed08bf21d2a22c435d65a2a"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://github.com/openmagi/homebrew-tap/releases/download/magi-agent-0.1.19"
+    sha256               arm64_tahoe:  "048731e441d1f1b7e4957a0c867a8671a21dfbb5f6deffb6cc6d9fcc0e7bbf98"
+    sha256 cellar: :any, x86_64_linux: "418956d98694499bdc0e8b3404f8f16384ee8e9f3ad6f594edce3794038b1b61"
+  end
+
   depends_on "python@3.13"
 
   on_macos do
