0eafd278cc6df041dc08b4ce7f4d93bf35d731f0
Kevin <kevin.hs.sohn@gmail.com>
2026-07-02 02:30:09 +0900

    magi-agent: 0.1.100

--- Formula/magi-agent.rb
+++ Formula/magi-agent.rb
@@ -3,8 +3,8 @@ class MagiAgent < Formula
 
   desc "Open Magi agent runtime and CLI"
   homepage "https://github.com/openmagi/magi-agent"
-  url "https://github.com/openmagi/magi-agent/releases/download/v0.1.99/magi_agent-0.1.99.tar.gz"
-  sha256 "c849df5c1e3a852bc200e21b98d3b68aa6d3db02a9df34bbfdfe79e6048f955c"
+  url "https://github.com/openmagi/magi-agent/releases/download/v0.1.100/magi_agent-0.1.100.tar.gz"
+  sha256 "ae9011f8012e08f8c4137c492b33ab9e2473c820fa298c027d36d05f1098801c"
   license "Apache-2.0"
 
   depends_on "python@3.13"
